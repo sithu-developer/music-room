08a67df7a63a3bceb26dfc0d659106bd94e77cca
unknown <sithunaing8209@gmail.com>
2026-07-02 23:58:33 +0630

    user room design changed

--- music-room-frontend/src/app/user/rooms/page.tsx
+++ music-room-frontend/src/app/user/rooms/page.tsx
@@ -2,9 +2,11 @@
 import NewRoom from "@/components/NewRoom";
 import { useAppSelector } from "@/store/hooks";
 import { RoomCategory } from "@/type/prisma";
-import { Box, Button, Chip, Typography } from "@mui/material";
+import { Box, Chip, IconButton, Typography } from "@mui/material";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import ChairRoundedIcon from '@mui/icons-material/ChairRounded';
+import UserSideBar from "@/components/UserSideBar";
 
 const RoomsPage = () => {
     const [ openNewRoom , setOpenNewRoom ] = useState<boolean>(false);
@@ -20,24 +22,27 @@ const RoomsPage = () => {
     } , [ categories ])
 
     return (
-        <Box sx={{ height : "100vh" ,  display : "flex" , flexDirection : "column" , alignItems : "center" , gap : "10px" , bgcolor : "primary.light" , p : "10px" }} >
-            <Box sx={{ display : "flex" , justifyContent : "space-between" , alignItems : "center" , width : "100%" , px : "30px" , py : "8px"}} >
-                <Typography variant="h4" >Rooms</Typography>
-                <Button variant="outlined" sx={{ borderColor : "white" , textTransform : "none"}} onClick={() => setOpenNewRoom(true)} ><Typography>Create Your Own</Typography></Button>
+        <Box sx={{ height : "100vh" ,  display : "flex" , flexDirection : "column" , alignItems : "center" , bgcolor : "primary.light" }} >
+            <Box sx={{ display : "flex" , justifyContent : "space-between" , alignItems : "center" , width : "100%" , px : "20px" , py : "8px" , bgcolor : "primary.main" }} >
+                <UserSideBar />
+                <Typography variant="h4" sx={{ ml : "60px"}} >Rooms</Typography>
+                <IconButton onClick={() => setOpenNewRoom(true)} >
+                    <ChairRoundedIcon sx={{ color : "whitesmoke" , fontSize : "30px"}} />
+                </IconButton>
             </Box>
-            <Box sx={{ display : "flex" , gap : "20px" , maxWidth : "100vw" , p : "10px 20px" , overflowX : "auto"}} >
+            <Box sx={{ display : "flex" , gap : "20px" , width : "100vw" , p : "0 30px 10px 30px" , overflowX : "auto" , ml : "90px" }} >
                 {categories.map(item => (
-                    <Chip key={item.id} onClick={() => setSelectedCategory(item)} label={item.name} clickable sx={{ color : "white" , border : (selectedCategory && item.id === selectedCategory.id ? "1px solid white": "") }} />
+                    <Chip key={item.id} onClick={() => setSelectedCategory(item)} label={item.name} clickable sx={{ color : "white" , borderTopRightRadius : "0" , borderTopLeftRadius : "0" , border : (selectedCategory && item.id === selectedCategory.id ? "1px solid white": "") , borderTop : "none" , bgcolor : (selectedCategory && item.id === selectedCategory.id ? "primary.main": "")  , ":hover" : { bgcolor : (selectedCategory && item.id === selectedCategory.id ? "primary.main": "") } }} />
                 )) }
             </Box>
-            <Box sx={{ width : "100%" , p : "10px" , display : "flex" , gap : "20px" , flexWrap : "wrap"}}>
+            <Box sx={{ width : "100%" , p : "10px 20px" , display : "flex" , gap : "20px" , flexWrap : "wrap"}}>
                 {selectedCategory && rooms.filter(item => item.roomCategoryId === selectedCategory.id).map(item => {
                     const roomImage = roomImages.find(roomImg => roomImg.id === item.currentRoomImageId )
                     if(roomImage)
                     return (
                         <Box key={item.id} sx={{ position : "relative" ,  display : "flex" , flexDirection : "column" , alignItems : "center" , height : "250px" , borderRadius : "10px" , overflow : "hidden" }} >
                             <Box sx={{ position : "absolute" , p : "5px 8px" }}>
-                                <Typography sx={{ background : "linear-gradient( 45deg  , #0c0b0b , #0c0b0b, #0c0b0b , #fff , #fff , #fff)", textShadow : "1px 1px 5px #ffffff" , fontWeight : "bold" , backgroundClip : "text" , color : "transparent" }} >{item.name}</Typography>
+                                <Typography sx={{ background : "linear-gradient( 45deg  , #0c0b0b , #0c0b0b, #0c0b0b , #fff , #fff , #fff)", textShadow : "0px 0px 10px #ffffff" , fontWeight : "bold" , backgroundClip : "text" , color : "transparent" }} >{item.name}</Typography>
                             </Box>
                             <Image alt="room image" src={roomImage.bgImageUrl} width={500} height={500} style={{ height : "100%" , width : "auto"}} />
                         </Box>
---
+++ music-room-frontend/src/components/UserSideBar.tsx
@@ -0,0 +1,68 @@
+"use-client"
+import { Box, ButtonBase, SvgIconProps } from "@mui/material";
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import { useState } from "react";
+import KeyboardArrowUpRoundedIcon from '@mui/icons-material/KeyboardArrowUpRounded';
+import WallpaperIcon from '@mui/icons-material/Wallpaper';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+
+const UserSideBar = () => {
+
+    const [ isOpenSideBar , setIsOpenSideBar ] = useState<boolean>(false);
+                console.log("1")
+
+    return (
+        <Box sx={{ position : "absolute" , zIndex : 1 , transition : "all 1s ease-in-out" , height : (isOpenSideBar ? "500px" : "85px") , bgcolor : "primary.dark" , display : "flex" , flexDirection : "column" , gap : "10px" , justifyContent : "end" , borderRadius : "0 0 30px 10px" , overflow : "hidden" , pb : "2px"}} >
+            {isOpenSideBar ? 
+            userSideBarItems.map(item => (
+                <Box component={ButtonBase} onClick={(e) => {
+                    e.stopPropagation();
+                }} key={item.id} sx={{ position : "relative" , zIndex : 2 , p : "8px"}} >
+                    <item.icon sx={{ color : "whitesmoke" , fontSize : "25px" }} />
+                </Box>
+            ))
+             : undefined}
+            <Box component={ButtonBase} onClick={() => setIsOpenSideBar(!isOpenSideBar)}  sx={{ transition : "all 0.3s linear" , height : (isOpenSideBar ? "40px" : "100%") , display : "flex" , alignItems : "end" , p : "8px"}} >
+                {isOpenSideBar ? 
+                <KeyboardArrowUpRoundedIcon sx={{ color : "whitesmoke" , fontSize : "25px" }}  />
+                :<KeyboardArrowDownRoundedIcon sx={{ color : "whitesmoke", fontSize : "25px" }} />}
+            </Box>
+        </Box>
+
+    )
+}
+
+export default UserSideBar;
+
+
+export interface UserSideBarItemType {
+    id : number;
+    name : string;
+    url : string;
+    icon : React.ComponentType<SvgIconProps>
+}
+
+const userSideBarItems : UserSideBarItemType[] = [
+
+    {
+        id : 2,
+        name : "Room Image",
+        icon : WallpaperIcon,
+        url : "/user/room-image"
+    },
+    {
+        id : 3,
+        name : "Music",
+        icon : MusicNoteIcon,
+        url : "/user/music"
+    },
+    {
+        id : 4,
+        name : "Settings",
+        icon : SettingsIcon,
+        url : "/user/settings"
+    },
+
+]
